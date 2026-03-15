0bcdc1834769aa52914fe527fae8a20413736f2f
Dhanush292006 <dhanushs99777@gmail.com>
2026-03-15 12:19:41 +0530

    Update App.py

--- App.py
+++ App.py
@@ -1,20 +1,16 @@
+
+
 import streamlit as st
 import google.generativeai as genai
 
-# Page title
-st.title("AI Chatbot")
-
-# Configure Gemini API
 genai.configure(api_key="AIzaSyDFN-L5LZ1uLJ5ohts3x5hn0uf9kZ2TrdE")
 
 model = genai.GenerativeModel("gemini-pro")
 
-# Chat input
-prompt = st.chat_input("Ask something...")
+st.title("Class AI")
+
+prompt = st.chat_input("Ask something")
 
 if prompt:
-    st.write("You:", prompt)
-
     response = model.generate_content(prompt)
-
-    st.write("AI:", response.text)
+    st.write(response.text)
